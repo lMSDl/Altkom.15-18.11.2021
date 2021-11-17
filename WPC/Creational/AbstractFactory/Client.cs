d564671c80638bab30355374667490d3441a5895
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-17 11:11:08 +0100

    AbstractFactory

---
+++ WPC/Creational/AbstractFactory/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.Creational.AbstractFactory
+{
+    class Client
+    {
+        public static void Execute()
+        {
+            Order order;
+            order = new Order(new HondaFactory(), nameof(ISedan), "compact");
+
+            order = new Order(new ToyotaFactory(), nameof(ISuv), "full");
+        }
+   }
+}

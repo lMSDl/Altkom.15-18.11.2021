76630e77e2455468f2eae69dbcf2b01d64a26fb6
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-17 10:54:52 +0100

    FacotryMethod

---
+++ WPC/Creational/FacotryMethod/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.Creational.FacotryMethod
+{
+    class Client
+    {
+        public static void Execute()
+        {
+            var elevator = new Elevator();
+
+            var action = "GoTo";
+            var floor = 3;
+               elevator.Execute(action, floor);
+
+            //IElevatorOperation elevatorOperation = null;
+            //switch (nameof(Elevator) + action)
+            //{
+            //    case nameof(ElevatorDown):
+            //        elevatorOperation = new ElevatorDown();
+            //        break;
+            //    case nameof(ElevatorUp):
+            //        elevatorOperation = new ElevatorUp();
+            //        break;
+            //}
+
+            //if (elevatorOperation != null)
+            //{
+            //    elevator.Execute(elevatorOperation, floor);
+            //}
+        }
+    }
+}
---
+++ WPC/Creational/FacotryMethod/Elevator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.Creational.FacotryMethod
+{
+    public class Elevator
+    {
+        private readonly Dictionary<string, IElevatorOperation> _commands;
+
+        public Elevator()
+        {
+            _commands = new Dictionary<string, IElevatorOperation>
+            {
+            { nameof(ElevatorGoTo), new ElevatorGoTo() },
+            { nameof(ElevatorDown), new ElevatorDown() },
+            { nameof(ElevatorUp), new ElevatorUp() },
+            };
+        }
+
+        public void Execute(IElevatorOperation operation, int floor)
+        {
+            operation?.Operate(floor);
+        }
+
+        public void Execute(string operation, int floor)
+        {
+            CreateAction(operation)?.Operate(floor);
+        }
+
+        public IElevatorOperation CreateAction(string action)
+        {
+            return _commands[action];
+
+            //switch (nameof(Elevator) + action)
+            //{
+            //    case nameof(ElevatorGoTo):
+            //        return new ElevatorGoTo();
+            //    case nameof(ElevatorDown):
+            //        return new ElevatorDown();
+            //    case nameof(ElevatorUp):
+            //        return new ElevatorUp();
+            //}
+            //return null;
+        }
+
+    }
+
+    public interface IElevatorOperation
+    {
+        void Operate(int floor);
+    }
+    public class ElevatorDown : IElevatorOperation
+    {
+        public void Operate(int floor)
+        {
+            Console.WriteLine($"Winda zatrzyma się na piętrze {floor} podczas jazdy w dół");
+        }
+    }
+    public class ElevatorUp : IElevatorOperation
+    {
+        public void Operate(int floor)
+        {
+            Console.WriteLine($"Winda zatrzyma się na piętrze {floor} podczas jazdy do góry");
+        }
+    }
+    public class ElevatorGoTo : IElevatorOperation
+    {
+        public void Operate(int floor)
+        {
+            Console.WriteLine($"Winda rusza na piętrzo {floor}");
+        }
+    }
+}

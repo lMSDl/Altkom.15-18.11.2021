b9f818822ec97cbc898590719e4bcb67b59f2674
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-18 09:52:24 +0100

    PrivateClassData

---
+++ WPC/Structural/ProvateClassData/PrivateClassData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.Structural.ProvateClassData
+{
+    public class PrivateClassData
+    {
+        private int _value1;
+        private int _value2;
+
+        public PrivateClassData(int value1, int value2)
+        {
+            _value1 = value1;
+            _value2 = value2;
+        }
+
+        public int GetValue1()
+        {
+            return _value1;
+        }
+        public int GetValue2()
+        {
+            return _value2;
+        }
+    }
+}
---
+++ WPC/Structural/ProvateClassData/SomeClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.Structural.ProvateClassData
+{
+    public class SomeClass
+    {
+        //private int _value1;
+        //private int _value2;
+        private PrivateClassData _privateClassData;
+
+        public SomeClass(int value1, int value2)
+        {
+            _privateClassData = new PrivateClassData(value1, value2);
+        }
+
+        public int GetValue1()
+        {
+            return _privateClassData.GetValue1();
+        }
+        public int GetValue2()
+        {
+            return _privateClassData.GetValue2();
+        }
+
+        public void DoSth()
+        {
+            //_value1 = 3;
+            //_value2 = 1;
+        }
+
+    }
+}

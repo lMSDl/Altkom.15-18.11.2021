7755abdcf08f7441c070f0fbb088d44f47608d99
Paweł Łęczycki <pawel@msd.info.pl>
2021-11-18 14:08:50 +0100

    Strategy

---
+++ WPC/Behavioral/Strategy/Client.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace WPC.Behavioral.Strategy
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            var calculator = new Calculator();
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                var split = line.Split(' ');
+                if (split.Length < 3)
+                    continue;
+
+                calculator.Strategy = GetCalcStrategy(split[1]);
+
+                if (float.TryParse(split[0], out float val1) && float.TryParse(split[2], out float val2))
+                {
+                    var result = calculator.Operate(val1, val2);
+                    Console.WriteLine(result);
+
+                    result = GetStrategy(split[1]).Invoke(val1, val2);
+                    Console.WriteLine(result);
+                }
+
+            }
+        }
+
+        private static ICalcStrategy GetCalcStrategy(string arg)
+        {
+            switch (arg)
+            {
+                case "*":
+                    return new MultiplyCalcStrategy();
+                case "/":
+                    return new DivideCalcStrategy();
+                case "+":
+                    return new PlusCalcStrategy();
+                case "-":
+                    return new MinusCalcStrategy();
+                default:
+                    return null;
+            }
+        }
+
+        private static Func<float, float, float> GetStrategy(string arg)
+        {
+            switch (arg)
+            {
+                case "*":
+                    return (x, y) => x * y;
+                case "/":
+                    return (x, y) => x / y;
+                case "+":
+                    return (x, y) => x + y;
+                case "-":
+                    return (x, y) => x - y;
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ WPC/Behavioral/Strategy/ICalcStrategy.cs
@@ -0,0 +1,39 @@
+﻿namespace WPC.Behavioral.Strategy
+{
+    public interface ICalcStrategy
+    {
+        float Calculate(float value1, float value2);
+    }
+
+    public class PlusCalcStrategy : ICalcStrategy
+    {
+        public float Calculate(float value1, float value2)
+        {
+            return value1 + value2;
+        }
+    }
+
+    public class MinusCalcStrategy : ICalcStrategy
+    {
+        public float Calculate(float value1, float value2)
+        {
+            return value1 - value2;
+        }
+    }
+
+    public class MultiplyCalcStrategy : ICalcStrategy
+    {
+        public float Calculate(float value1, float value2)
+        {
+            return value1 * value2;
+        }
+    }
+
+    public class DivideCalcStrategy : ICalcStrategy
+    {
+        public float Calculate(float value1, float value2)
+        {
+            return value1 / value2;
+        }
+    }
+}
